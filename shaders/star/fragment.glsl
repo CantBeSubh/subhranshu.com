589ec435c7f70736dbf505c07dc809c0240b9e0e
Subhranshu Pati <subhranshupati0412@gmail.com>
2024-03-25 00:09:18 +0530

    yep nm

--- shaders/star/fragment.glsl
+++ shaders/star/fragment.glsl
@@ -3,6 +3,7 @@ varying vec3 vColor;
 uniform vec3 fogColor;
 uniform float fogNear;
 uniform float fogFar;
+uniform float uTime;
 
 void main()
 {
@@ -10,6 +11,7 @@ void main()
     float strength=distance(gl_PointCoord,vec2(0.5));
     strength=1.0-strength;
     strength=pow(strength,10.0);
+    //strength = strength*sin(uTime*0.2);
 
     vec3 color=mix(vec3(0.0),vColor,strength);
     gl_FragColor=vec4(color,1.0);
