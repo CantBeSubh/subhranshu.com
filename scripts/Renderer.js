6360d8c95ce9dfaa22b2bd06c253d2c3b604249c
Subhranshu Pati <subhranshupati0412@gmail.com>
2024-03-24 21:09:22 +0530

    cleanup

--- scripts/Renderer.js
+++
@@ -1,8 +0,0 @@
-import * as THREE from "three";
-
-const canvas = document.getElementById("canvas");
-const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
-renderer.setPixelRatio(Math.min(window.devicePixelRatio), 2);
-renderer.setSize(window.innerWidth, window.innerHeight);
-
-export default renderer;
