bbdb9b87fc0de747a26a0637eb2bfcefdbf3ddf9
Subhranshu Pati <subhranshupati0412@gmail.com>
2024-03-25 16:03:01 +0530

    cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import Dock from "@/components/common/dock";
 import { Galaxy } from "@/components/common/galaxy";
 
 export default function Home() {
   return (
     <div className='h-screen'>
       <Galaxy />
-      <Dock />
     </div>
   )
 }
