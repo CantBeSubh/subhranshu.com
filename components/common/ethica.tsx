64207bfad673e5b29065b079c057a1bb3601c92a
Subhranshu Pati <subhranshupati0412@gmail.com>
2024-03-25 18:26:39 +0530

    ethica logo link

--- components/common/ethica.tsx
+++ components/common/ethica.tsx
@@ -1,14 +1,18 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export const Ethica = () => {
     return (
-        <div className="font-thin w-fit absolute top-4 right-4 z-[99] text-white p-1 bg-white rounded-lg">
+        <Link
+            href="https://www.heyethica.com/"
+            className="font-thin w-fit absolute top-4 right-4 z-[99] text-white p-1 bg-white rounded-lg"
+        >
             <Image
                 src="/Logos/EthicaLogo.svg"
                 width={30}
                 height={30}
                 alt="Ethica Logo"
             />
-        </div>
+        </Link>
     );
 };
