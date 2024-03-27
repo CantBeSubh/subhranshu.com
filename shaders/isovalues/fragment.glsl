513a6c3fde6574972f04e9c5fa71f1ce0bccc73b
Subhranshu Pati <subhranshupati0412@gmail.com>
2024-03-27 22:20:55 +0530

    idk

--- shaders/isovalues/fragment.glsl
+++ shaders/isovalues/fragment.glsl
@@ -9,7 +9,7 @@ float noise3( vec3 x ) {
 
     f = f*f*(3.-2.*f);  // or smoothstep     // to make derivative continuous at borders
 
-    #define hash3(p)  fract(sin(1e3*dot(p,vec3(1,57,-13.7)))*4375.5453)        // rand
+#define hash3(p)  fract(sin(1e3*dot(p,vec3(1,57,-13.7)))*4375.5453)        // rand
     
     return mix( mix(mix( hash3(p+vec3(0,0,0)), hash3(p+vec3(1,0,0)),f.x),       // triilinear interp
                     mix( hash3(p+vec3(0,1,0)), hash3(p+vec3(1,1,0)),f.x),f.y),
@@ -19,19 +19,16 @@ float noise3( vec3 x ) {
 
 #define noise(x) (noise3(x)+noise3(x+11.5)) / 2. // pseudoperlin improvement from foxes idea 
 
-void mainImage( out vec4 O, vec2 U ) // ------------ draw isovalues
-{ 
+void main() {
+    vec4 color;
     vec2 R = iResolution.xy;
-    float n = noise(vec3(U*8./R.y, .1*iTime)), v = sin(6.28*10.*n), t = iTime;
+    float n = noise(vec3(gl_FragCoord.xy*8./R.y, .1*iTime));
+    float v = sin(6.28*10.*n);
+    float t = iTime;
     
     v = smoothstep(1.,0., .5*abs(v)/fwidth(v));
     
-	O = mix( exp(-33./R.y )* texture( iChannel0, (U+vec2(1,sin(t)))/R), .5+.5*sin(12.*n+vec4(0,2.1,-2.1,0)), v );
+	vec4 O = mix( exp(-33./R.y )* texture( iChannel0, (gl_FragCoord.xy+vec2(1,sin(t)))/R), .5+.5*sin(12.*n+vec4(0,2.1,-2.1,0)), v );
 
+    gl_FragColor = O;
 }
-void main() {
-    vec4 color;
-    mainImage(color, gl_FragCoord.xy);
-    gl_FragColor = color;
-}
-
--- shaders/isovalues/vertex.glsl
+++ shaders/isovalues/vertex.glsl
@@ -1,3 +1,7 @@
+uniform vec2 iResolution;
+uniform sampler2D iChannel0;
+
 void main() {
-    gl_Position = vec4(position, 1.0);
+	// gl_Position = texture( iChannel0, position.xy / iResolution.xy);
+	gl_Position = vec4(position, 1.0);
 }
