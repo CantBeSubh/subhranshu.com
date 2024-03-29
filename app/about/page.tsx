d3c186723ed8889c0fca5dea14760c9868c8399e
Subhranshu Pati <subhranshupati0412@gmail.com>
2024-03-29 08:25:03 +0530

    refactor

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,5 @@
 import { IsoValues } from "@/components/common/isovalues"
-import BentoGrid from "@/components/ui/bento-grid"
-import BentoCard from "@/components/ui/bento-grid/bento-card"
+import { BentoCard, BentoGrid } from "@/components/ui/bento-grid"
 
 const AboutPage = () => {
     return (
@@ -10,15 +9,13 @@ const AboutPage = () => {
             </div>
             <div className="z-10 p-4 border-2 border-black rounded-lg backdrop-blur-3xl h-[80%] w-[90%]">
                 {/* Bento Grid */}
-                <BentoGrid rows={3} cols={3}>
+                <BentoGrid rows={3} cols={4}>
                     <BentoCard col={2} row={2}>About ME</BentoCard>
                     <BentoCard>1</BentoCard>
                     <BentoCard>2</BentoCard>
                     <BentoCard>3</BentoCard>
                     <BentoCard>4</BentoCard>
                     <BentoCard col={2}>OKOK</BentoCard>
-                    <BentoCard>5</BentoCard>
-                    <BentoCard>6</BentoCard>
                     <BentoCard col={2}>OKOK</BentoCard>
                     <BentoCard>5</BentoCard>
                     <BentoCard>6</BentoCard>
--- components/ui/bento-grid/index.tsx
+++ components/ui/bento-grid/index.tsx
@@ -1,15 +1,37 @@
-import { ReactNode } from "react"
+'use client'
+import { ReactNode } from "react";
 
 type BentoGridProps = {
     children: ReactNode,
     rows: number,
     cols: number
 }
-export default function BentoGrid(props: BentoGridProps) {
-    const { cols, rows, children } = props
+
+type BentoCardProps = {
+    row?: number;
+    col?: number;
+    children: ReactNode;
+};
+
+const BentoCard = (props: BentoCardProps) => {
+    const { row, col, children } = props;
+    const cardClass = `col-span-${col ?? 1} row-span-${row ?? 1} backdrop-blur-3xl rounded-lg border-2 border-black flex items-center justify-center w-full h-full text-black cursor-pointer`;
     return (
-        <div className={`grid grid-cols-${cols} grid-rows-${rows} gap-4 p-4 h-full w-full`}>
+        <div className={cardClass}>
+            {children ?? 'PLACEHOLDER'}
+        </div>
+    );
+};
+
+
+const BentoGrid = (props: BentoGridProps) => {
+    const { cols, rows, children } = props
+    const gridClass = `grid grid-cols-${cols} grid-rows-${rows} gap-4 p-4 h-full w-full`
+    return (
+        <div className={gridClass}>
             {children}
         </div>
     )
 }
+
+export { BentoCard, BentoGrid };
