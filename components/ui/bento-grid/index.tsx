cfc2ea00a6ab58c8981176ba36cd71d82ca1bc8c
Subhranshu Pati <subhranshupati0412@gmail.com>
2024-03-29 08:28:48 +0530

    working

--- components/ui/bento-grid/index.tsx
+++ components/ui/bento-grid/index.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 
 type BentoGridProps = {
     children: ReactNode,
@@ -15,7 +15,7 @@ type BentoCardProps = {
 
 const BentoCard = (props: BentoCardProps) => {
     const { row, col, children } = props;
-    const cardClass = `col-span-${col ?? 1} row-span-${row ?? 1} backdrop-blur-3xl rounded-lg border-2 border-black flex items-center justify-center w-full h-full text-black cursor-pointer`;
+    const [cardClass] = useState(() => `col-span-${col ?? 1} row-span-${row ?? 1} backdrop-blur-3xl rounded-lg border-2 border-black flex items-center justify-center w-full h-full text-black cursor-pointer`)
     return (
         <div className={cardClass}>
             {children ?? 'PLACEHOLDER'}
