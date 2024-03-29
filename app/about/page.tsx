8432cc0573339818172fa5a30ea2a7c9d588b883
Subhranshu Pati <subhranshupati0412@gmail.com>
2024-03-29 09:57:56 +0530

    idk

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -13,26 +13,35 @@ const AboutPage = () => {
                 {/* Bento Grid */}
                 <BentoGrid rows={4} cols={4}>
                     <BentoCard col={2} row={2}>
-                        <div className="flex p-2 bg-red-100 size-full">
+                        {/* TODO: Fix height issue */}
+                        <div className="flex p-4 size-full justify-between">
                             {/* PFP */}
-                            <div className="w-fit border-r border-black p-2 flex flex-col items-start justify-between">
-                                <Image
-                                    src="/Images/pfp.png"
-                                    width={75}
-                                    height={75}
-                                    alt="PFP"
-                                    className="rounded-full border-[0.5px] border-black"
-                                />
-                                <div className="text-xl font-bold">
-                                    Subhranshu Pati
+                            <div className="w-2/5 pl-2 flex flex-col items-start justify-between">
+                                <div className="space-y-2 w-full">
+                                    <Image
+                                        src="/Images/pfp.png"
+                                        width={100}
+                                        height={100}
+                                        alt="PFP"
+                                        className="rounded-full border-[0.5px] border-black mb-4"
+                                    />
+                                    <div className="border-b border-neutral-300/40 border-[0.1px] w-[90%]" />
+                                    <div className="">
+                                        <div className="text-xl font-bold"> Subhranshu Pati </div>
+                                        <div className="text-sm"> Software Engineer </div>
+                                    </div>
                                 </div>
-                                <div className="text-sm">
-                                    Software Engineer
-                                </div>
+
                                 <Link href='/' className="underline">Read.cv</Link>
                             </div>
-                            <div className="flex flex-col items-center justify-between">
+                            <div className="w-3/5 text-sm">
+                                I was born in a small town in western Germany, and I grew up in an ever-growing age of technology. My whole life, I was curious about how technology evolves and how it can enhance our everyday life.
 
+                                A few years ago, I started to learn more about design. For me, it was important to bring my ideas to life, so I self-taught myself how to code and learned more about Design Engineering . In early 2023, I started my bachelor in Interaction Design at the University of Applied Sciences in Schwaebisch Gmuend.
+
+                                Right now i am working at
+                                zwoeins marketing
+                                and I am always working on side projects to improve my skills and expand my knowledge in the field of software development.
                             </div>
                         </div>
                     </BentoCard>
--- components/ui/bento-grid/index.tsx
+++ components/ui/bento-grid/index.tsx
@@ -15,7 +15,7 @@ type BentoCardProps = {
 
 const BentoCard = (props: BentoCardProps) => {
     const { row, col, children } = props;
-    const [cardClass] = useState(() => `col-span-${col ?? 1} row-span-${row ?? 1} backdrop-blur-3xl rounded-lg border-2 border-black flex items-center justify-center w-full h-full text-black`)
+    const [cardClass] = useState(() => `col-span-${col ?? 1} row-span-${row ?? 1} backdrop-blur-3xl rounded-lg border-2 border-black flex items-center justify-center w-full h-full`)
     return (
         <div className={cardClass}>
             {children ?? 'PLACEHOLDER'}
