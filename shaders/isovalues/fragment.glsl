6eff35b34064a34eb49890686068e5d69ad60480
Subhranshu Pati <subhranshupati0412@gmail.com>
2024-04-13 22:46:55 +0530

    new bg color

--- shaders/isovalues/fragment.glsl
+++ shaders/isovalues/fragment.glsl
@@ -5,7 +5,8 @@ uniform float uTransitionTime;
 
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
-    float strength = smoothstep(0.0, uTransitionTime, uTime) * 1.;
+    // float strength = smoothstep(0.0, uTransitionTime*2.0, uTime) * 1.;
+    float strength = abs(sin(uTime*0.15) * .4);
 	vec2 p = -1.0 + 2.0 * fragCoord.xy / uResolution.xy;
     float x = p.x;
     float y = p.y;
@@ -13,9 +14,9 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
     float mov1 = y / 0.9 +  uTime;
     float mov2 = x / 0.2;
     float c1 = abs(sin(mov1+uTime)/2.+mov2/2.-mov1-mov2+uTime) * strength;
-    float c2 = abs(sin(c1+sin(mov0/1000.+uTime)+sin(y/40.+uTime)+sin((x+y)/100.)*3.)) * strength;
+    float c2 = abs(sin(c1+sin(mov0/1000.+uTime*0.0001)+sin(y/40.+uTime*0.0001)+sin((x+y)/100.)*3.)) * strength;
     float c3 = abs(sin(c2+cos(mov1+mov2+c2)+cos(mov2)+sin(x/1000.))) * strength;
-    fragColor = vec4(c1,c2,c3,1);
+    fragColor = vec4(c2,c2,c2,1);
 	
 }
 
