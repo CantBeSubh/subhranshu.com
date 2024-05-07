402edd21fec349c9c0e4aaafdf4957f23d12fb9f
Subhranshu Pati <subhranshupati0412@gmail.com>
2024-05-07 10:01:42 +0530

    Metrics

---
+++ app/metrics/index.tsx
@@ -0,0 +1,11 @@
+import MicrosoftClarity from "./clarity"
+import GoogleAnalytics from "./google-analytics"
+
+const Metrics = () => (
+    <>
+        <GoogleAnalytics />
+        <MicrosoftClarity />
+    </>
+)
+
+export default Metrics
