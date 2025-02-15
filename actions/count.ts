0036010384cd820b745381e157b13f567f9bd418
Subhranshu Pati <subhranshupati0412@gmail.com>
2025-02-16 00:43:33 +0530

    fix: cookie store

--- actions/count.ts
+++ actions/count.ts
@@ -2,50 +2,60 @@
 
 import { db } from "@/lib/db";
 import { revalidatePath } from "next/cache";
+import { cookies } from "next/headers";
 
 export const getLikeCount = async () => {
-    const count = await db.counter.findFirst({
-        where: {
-            id: 1,
-        },
-    });
+  const count = await db.counter.findFirst({
+    where: {
+      id: 1,
+    },
+  });
 
-    return count?.likeCount ?? 96;
+  return count?.likeCount ?? 96;
 };
 
 export const getVisitCount = async () => {
-    const count = await db.counter.findFirst({
-        where: {
-            id: 1,
-        },
-    });
+  const count = await db.counter.findFirst({
+    where: {
+      id: 1,
+    },
+  });
 
-    return count?.visitCount ?? 96;
+  return count?.visitCount ?? 96;
 };
 
 export const incrementLikeCount = async () => {
-    await db.counter.update({
-        where: {
-            id: 1,
-        },
-        data: {
-            likeCount: {
-                increment: 1,
-            },
-        },
-    });
-    revalidatePath("/", "page");
+  await db.counter.update({
+    where: {
+      id: 1,
+    },
+    data: {
+      likeCount: {
+        increment: 1,
+      },
+    },
+  });
+  revalidatePath("/", "page");
 };
 
 export const incrementVisitCount = async () => {
+  const cookieStore = await cookies();
+  const hasVisited = cookieStore.get("hasVisited");
+  console.log("hasVisited", hasVisited);
+
+  if (!hasVisited) {
     await db.counter.update({
-        where: {
-            id: 1,
+      where: {
+        id: 1,
+      },
+      data: {
+        visitCount: {
+          increment: 1,
         },
-        data: {
-            visitCount: {
-                increment: 1,
-            },
-        },
+      },
     });
+    cookieStore.set("hasVisited", "true", {
+      expires: new Date(Date.now() + 1000 * 60 * 10), // 10 minutes
+    });
+  }
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import { incrementVisitCount } from "@/actions/count";
 import Metrics from "@/app/metrics";
 import Footer from "@/components/footer";
 import Header from "@/components/header";
@@ -12,41 +11,39 @@ import "./globals.css";
 const inter = Montserrat({ subsets: ["latin"] });
 
 export const metadata = {
-    title: "Subh",
-    description: "Subhranshu hates CSS",
+  title: "Subh",
+  description: "Subhranshu hates CSS",
 };
 
 export default async function RootLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    incrementVisitCount();
+  return (
+    <html lang="en" suppressHydrationWarning={true}>
+      <body
+        className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-black dark:text-gray-50 dark:text-opacity-90 dark:bg-dot-white/[0.1] bg-dot-black/[0.2]`}
+      >
+        <Metrics />
+        <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
+        <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
 
-    return (
-        <html lang="en" suppressHydrationWarning={true}>
-            <body
-                className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-black dark:text-gray-50 dark:text-opacity-90 dark:bg-dot-white/[0.1] bg-dot-black/[0.2]`}
-            >
-                <Metrics />
-                <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
-                <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
-
-                <ThemeProvider
-                    attribute="class"
-                    defaultTheme="system"
-                    enableSystem
-                    disableTransitionOnChange
-                >
-                    <ActiveSectionContextProvider>
-                        <Header />
-                        {children}
-                        <Footer />
-                        <Toaster position="top-right" />
-                        <ThemeSwitch />
-                    </ActiveSectionContextProvider>
-                </ThemeProvider>
-            </body>
-        </html>
-    );
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <ActiveSectionContextProvider>
+            <Header />
+            {children}
+            <Footer />
+            <Toaster position="top-right" />
+            <ThemeSwitch />
+          </ActiveSectionContextProvider>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
 }
