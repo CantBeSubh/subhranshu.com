6b2373637b4c509c4a70f061806cd4aa1bcc9f01
Subhranshu Pati <subhranshupati0412@gmail.com>
2026-06-21 20:19:56 +0530

    feat: add Prisma client for existing Counter table

---
+++ src/lib/db.ts
@@ -0,0 +1,11 @@
+import { PrismaClient } from "@prisma/client";
+
+const db = new PrismaClient();
+
+const globalForPrisma = global as unknown as { prisma: typeof db };
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = db;
+}
+
+export { db };
